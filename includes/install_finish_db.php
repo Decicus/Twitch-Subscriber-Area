d1c47dfd99d4d4b6815da57047932d9a07605e88
Alex Thomassen <alex@thomassen.xyz>
2015-04-24 15:50:02 +0200

    Fix for magic_quotes_gpc

--- includes/install_finish_db.php
+++ includes/install_finish_db.php
@@ -5,9 +5,9 @@
         exit();
     }
     $fetchTitle = mysqli_fetch_array( mysqli_query( $con, "SELECT meta_value FROM " . TSA_DB_PREFIX . "settings WHERE meta_key='title';" ) );
-    $title = $fetchTitle['meta_value'];
+    $title = stripslashes( $fetchTitle['meta_value'] );
     $fetchMainText = mysqli_fetch_array( mysqli_query( $con, "SELECT meta_value FROM " . TSA_DB_PREFIX . "settings WHERE meta_key='main_text';" ) );
-    $main_text = $fetchMainText['meta_value'];
+    $main_text = stripslashes( $fetchMainText['meta_value'] );
     if( !$title || !$main_text ) {
         echo 'MySQL error - TSA cannot initialize: ' . mysqli_error( $con );
         exit();

009fa63e388b7986bfb4f527038babda9da14e69
Alex Thomassen <alex@thomassen.xyz>
2015-05-01 02:30:50 +0200

    Fixed 'drunk' programming for whitelisting

--- includes/admin/whitelist.php
+++ includes/admin/whitelist.php
@@ -1,10 +1,9 @@
 <?php
-    $checkWhitelist = mysqli_query( $con, "SHOW TABLES LIKE " . TSA_DB_PREFIX . "whitelist;" );
+    $checkWhitelist = mysqli_query( $con, "SHOW TABLES LIKE '" . TSA_DB_PREFIX . "whitelist';" );
     if( mysqli_num_rows( $checkWhitelist ) == 0 ) {
         // For v1.0 support
         $createWhitelist = mysqli_query( $con, "CREATE TABLE " . TSA_DB_PREFIX . "whitelist( id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name varchar(25), uid int UNIQUE );" );
     }
-    $fetchWhitelist = mysqli_fetch_array( mysqli_query( $con, "SELECT name,uid FROM " . TSA_DB_PREFIX . "whitelist;" ) );
     
     if( !empty( $_POST['addWhitelist'] ) ) {
         $newWLName = mysqli_real_escape_string( $con, $_POST['addWhitelist'] );
@@ -35,8 +34,8 @@
     }
     
     if( !empty( $_POST['delWhitelist'] ) ) {
-        $delWLUID = intval( $con, $_POST['delWhitelist'] );
-        $checkDelWL = mysqli_query( $con, "SELECT name FROM " . TSA_DB_PREFIX . "whitelist WHERE uid='$delWLUID' LIMIT 1;" );
+        $delWLUID = intval( $_POST['delWhitelist'] );
+        $checkDelWL = mysqli_fetch_array( mysqli_query( $con, "SELECT name FROM " . TSA_DB_PREFIX . "whitelist WHERE uid='$delWLUID' LIMIT 1;" ) );
         if( $checkDelWL ) {
             $delWLName = $checkDelWL['name'];
             $delWLUser = mysqli_query( $con, "DELETE FROM " . TSA_DB_PREFIX . "whitelist WHERE uid='$delWLUID';" );
@@ -55,6 +54,7 @@
             <?php
         }
     }
+    $getWhitelist =  mysqli_query( $con, "SELECT name,uid FROM " . TSA_DB_PREFIX . "whitelist;" );
 ?>
 <div class="panel panel-success">
     <div class="panel-heading">Add whitelisted user</div>
@@ -78,7 +78,7 @@
                 <select name="delWhitelist" id="delWhitelist" class="form-control">
                     <option>Select user</option>
                     <?php
-                        while( $row = mysqli_fetch_array( $fetchWhitelist ) ) {
+                        while( $row = mysqli_fetch_array( $getWhitelist ) ) {
                             ?>
                             <option value="<?php echo $row['uid']; ?>"><?php echo $row['name']; ?></option>
                             <?php
@@ -86,7 +86,7 @@
                     ?>
                 </select>
             </div>
+            <button type="submit" class="btn btn-danger" onclick='confirm( "Are you sure you want to remove this user from the whitelist?" );' )>Remove from whitelist</button>
         </form>
-        <button type="submit" class="btn btn-danger" onclick='confirm( "Are you sure you want to remove this user from the whitelist?" );' )>Remove from whitelist</button>
     </div>
 </div>
